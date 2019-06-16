b666baf430bd051ca42f88f4416e783c8673ace4
zurgeg <46549042+zurgeg@users.noreply.github.com>
2019-06-16 10:47:49 -0400

    Added 2 player full functionality

--- src/main/onlinechess.py
+++ src/main/onlinechess.py
@@ -53,4 +53,42 @@ elif mode.upper() == "2 PLAYERS":
     gameid = json.loads(json_string)['game_id']
     print("Use this ID to connect via ChessConnet")
     print("Initilazed.")
+    while True:
+        cmd = input("You can:\nget moves\nmove\n>")
+        brd = os.popen('curl --location --request POST "http://chess-api-chess.herokuapp.com/api/v1/chess/two/fen" \
+          --header "Content-Type: application/x-www-form-urlencoded" \
+          --data "game_id=' + gameid + '"')
+        chck = os.popen('curl --location --request POST "http://chess-api-chess.herokuapp.com/api/v1/chess/two/check" \
+          --header "Content-Type: application/x-www-form-urlencoded" \
+          --data "game_id=' + gameid + '"')
+        json_string = chck.read().replace("'", "\"")
+        checker = json.loads(json_string)
+        print(brd.read())
+        #print(chck.read())
+        if checker['status'] == 'game continues':
+            pass
+        elif checker['status'] == 'checkmate':
+            print("Checkmate!!")
+            break
+            
+
+        if cmd.lower() == "get moves":
+            pos = input("Enter the position of the piece you want to get moves for: ")
+            getmoves = os.popen('curl --location --request POST "http://chess-api-chess.herokuapp.com/api/v1/chess/two/moves" \
+              --header "Content-Type: application/x-www-form-urlencoded" \
+              --data "position=' + pos + '&game_id="' + gameid)
+            print(getmoves.read())
+        elif cmd.lower() == "move":
+            FROM = input("Enter the piece that you want to move: ")
+            TO = input("Enter where you want to move it: ")
+            MOVE = os.popen('curl --location --request POST "http://chess-api-chess.herokuapp.com/api/v1/chess/two/move" \
+          --header "Content-Type: application/x-www-form-urlencoded" \
+          --data "from=' + FROM + '&to=' + TO + '&game_id=' + gameid + '"')
+            print(MOVE.read())
+            json_acceptable_string = MOVE.read().replace("'", "\"")
+            MOVEP = json.loads(json_acceptable_string)
+            if MOVEP["status"] == "error: invalid move!":
+                print("Error: Invalid Move!")
+            
+
         
