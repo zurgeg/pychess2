9054848d41154eb1143f81e201db98a5bafdcfab
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-07 17:56:57 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,8 +6,4 @@ print('Outbound testing succesful')
 print('Cythoning...')
 os.system('cython onlinechess.py')
 print('Cythoned 1 file')
-print('Output:')
-f = open('onlinechess.cpp')
-print(f.read())
-print('Outputted {} lines'.format(len(f.readlines())))
 
