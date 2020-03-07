90e24f658d85e547ebe4fc4c8a18212571bb4066
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-03-07 17:50:26 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,13 @@
 import requests
+import os
 a = requests.get('https://github.com')
   
+print('Outbound testing succesful')
+print('Cythoning...')
+os.system('cython onlinechess.py -o online.cpp')
+print('Cythoned 1 file')
+print('Output:')
+f = open('online.cpp')
+print(f.read())
+print('Outputted {} lines'.format(len(f.readlines())))
+
